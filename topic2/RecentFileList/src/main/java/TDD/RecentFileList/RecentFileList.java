1ea5ce28b9f7a7de477cb44a251e8d829218cb94
nico <nicoperetti.16@gmail.com>
2016-01-20 21:09:38 -0300

    Topic2-1: done

--- topic2/RecentFileList/src/main/java/TDD/RecentFileList/RecentFileList.java
+++ topic2/RecentFileList/src/main/java/TDD/RecentFileList/RecentFileList.java
@@ -6,14 +6,31 @@ import java.util.*;
 public class RecentFileList{
     
 	private List<File> recentfilelist;
+	private int length;
 	
-	public void RecentFileList() {
+	public RecentFileList() {
 		recentfilelist = new ArrayList<File>();
+		length = 15;
 	}
 	
 	public int size() {
-		return recentfilelist.size();
+		int result = 0;
+		if (recentfilelist != null) {
+			result = recentfilelist.size();
+		}
+		return result;
 	}
 	
-	
+	public void OpenFile(File file) {
+		if (recentfilelist.contains(file)) {
+			recentfilelist.remove(file);
+		}
+		recentfilelist.add(0, file);
+		if (recentfilelist.size() > length) {
+			recentfilelist.remove(length - 1);
+		}
+	}
+	public int getlength() {
+		return length;
+	}
 }
--- topic2/RecentFileList/src/test/java/TDD/RecentFileList/RecentFileListTest.java
+++ topic2/RecentFileList/src/test/java/TDD/RecentFileList/RecentFileListTest.java
@@ -1,6 +1,9 @@
 package TDD.RecentFileList;
 
 import static org.junit.Assert.*;
+
+import java.io.File;
+
 import org.junit.Test;
 
 public class RecentFileListTest {
@@ -8,7 +11,26 @@ public class RecentFileListTest {
 	@Test
 	public void testEmptyRecentFileList() {
         RecentFileList Filelist = new RecentFileList();
-        /*AssertEquals(0, Filelist.size());*/
+        assertEquals(0, Filelist.size());
     }
 	
+	@Test
+	public void testOpenFile() {
+		RecentFileList Filelist = new RecentFileList();
+		File file = new File("~/file1.txt");
+		Filelist.OpenFile(file);
+		assertEquals(1, Filelist.size());
+	}
+	
+	@Test
+	public void testOpenFilefull() {
+		RecentFileList Filelist = new RecentFileList();
+		int len = Filelist.getlength();
+		File file;
+		for(int i = 0; i < len; i++) {
+			file = new File("~/file" + i + ".txt");
+			Filelist.OpenFile(file);
+		}
+		assertEquals(len, Filelist.size());
+	}
 }
