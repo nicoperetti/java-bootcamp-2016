ff3220891ad696ee65c7edee72fc8597154de8e5
nico <nicoperetti.16@gmail.com>
2016-01-25 18:27:47 -0300

    Topic3-5: document a UserService

--- topic3/UserService/src/main/java/CRUD/UserService/UserServiceI.java
+++ topic3/UserService/src/main/java/CRUD/UserService/UserServiceI.java
@@ -2,12 +2,29 @@ package CRUD.UserService;
 
 
 public interface UserServiceI{
-
+	/* Create a new user and store it in a mock db 
+	 * name  -- user name
+	 * dob   -- user date of birth
+	 * email -- user email
+	 * return the user.
+	 * */
 	public User createUser(String name, String dob, String email);
 	
+	/* Get information about the user, read from the mock database
+	 * user -- the user
+	 * return a string with the information
+	 *  */
 	public String readUser(User user);
 	
+	/* Update a User and store it in the mock db
+	 * user    -- the user that will be updating
+	 * field   -- the field that I want to update
+	 * newdata -- the new data
+	 * */
 	public void updateUser(User user, String field, String newdata);
 	
+	/* Remove the user from the mock db
+	 * user -- the user that I want to delete.
+	 * */
 	public void deleteUser(User user);
 }
